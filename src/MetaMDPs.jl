0265ba6c9d99505b8e8bc939daa07ce96f541a21
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2022-11-12 02:20:53 -0500

    0.2.0. removed gamma, horizon from metaMDP def.

--- src/MetaMDPs.jl
+++ src/MetaMDPs.jl
@@ -2,25 +2,23 @@ module MetaMDPs
 
 using Random
 using MDPs
-import MDPs: state_space, action_space, discount_factor, horizon, action_meaning, state, action, reward, reset!, step!, in_absorbing_state, visualize, factory_reset!
+import MDPs: state_space, action_space, action_meaning, state, action, reward, reset!, step!, in_absorbing_state, visualize, factory_reset!
 
 export MetaMDP
 
 mutable struct MetaMDP{S, A} <: AbstractMDP{S, A}
     tasks::Vector{AbstractMDP{S, A}}
-    horizon::Int
 
     task::AbstractMDP{S, A}
     state::S
     action::A
     reward::Float64
-    t::Int
 
-    function MetaMDP(tasks, horizon)
+    function MetaMDP(tasks)
         task = tasks[1]
         sspace, aspace = state_space(task), action_space(task)
         S, A = eltype(sspace), eltype(aspace)
-        return new{S, A}(tasks, horizon, task,  state(task), action(task), reward(task), 0)
+        return new{S, A}(tasks, task,  state(task), action(task), reward(task))
     end
 end
 
@@ -33,9 +31,6 @@ end
 state_space(mm::MetaMDP) = state_space(mm.task)
 action_space(mm::MetaMDP) = action_space(mm.task)
 action_meaning(mm::MetaMDP{S,A}, a::A) where {S,A} = action_meaning(mm.task, a)
-discount_factor(mm::MetaMDP) = 0.99
-horizon(mm::MetaMDP) = mm.horizon
-
 
 
 state(mm::MetaMDP) = mm.state
@@ -47,7 +42,6 @@ function reset!(mm::MetaMDP; rng::AbstractRNG=Random.GLOBAL_RNG)::Nothing
     @debug "Sampled new task" mm.task
     factory_reset!(mm.task)
     reset!(mm.task; rng=rng)
-    mm.t = 0
     mm.state = copy(state(mm.task))
     mm.action = action(mm.task)
     mm.reward = reward(mm.task)
@@ -56,7 +50,6 @@ end
 
 function step!(mm::MetaMDP{S, A}, a::A; rng::AbstractRNG=Random.GLOBAL_RNG)::Nothing where {S, A}
     @assert a ∈ action_space(mm)
-    mm.t += 1
     step!(mm.task, a; rng=rng)
     mm.state = copy(state(mm.task))
     mm.action = a
